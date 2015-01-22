de06c4bd1ad9a709b9165b795703c57fd4ec453a
stuqs <nanorhist@ukr.net>
2015-01-22 16:29:35 +0200

    Edit model, add language settings, advnaced admin settings

--- Telephone/admin.py
+++ Telephone/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
-from Telephone.models import *
+from Telephone.models import ProsecutorsOffice, Department, Division, Employee
 
 
-admin.site.register(ProsecutorsOffice)
-admin.site.register(Department)
-admin.site.register(Division)
-admin.site.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('surname', 'name', 'patronymic', 'position', 'work_telephone')
+    search_fields = ('surname', 'name', 'patronymic', 'position', 'work_telephone')
+    ordering = ('surname',)
+
+
+class ProsecutorsOfficeAdmin(admin.ModelAdmin):
+    search_fields = ('surname', 'name', 'patronymic', 'position', 'work_telephone')
+    filter_horizontal = ('department', 'division', 'employees')
+
+
+class DepartmentAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    filter_horizontal = ('division', 'employees')
+
+
+
+class DivisionAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    filter_horizontal = ('employees',)
+
+
+
+
+admin.site.register(ProsecutorsOffice, ProsecutorsOfficeAdmin)
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Division, DivisionAdmin)
+admin.site.register(Employee, EmployeeAdmin)
--- Telephone/models.py
+++ Telephone/models.py
@@ -5,16 +5,16 @@ class Employee(models.Model):
     """
     Модель для всех сотрудников
     """
-    name = models.CharField(blank=True, max_length=30, verbose_name="Ім'я")
-    surname = models.CharField(blank=True, max_length=30, verbose_name='Фамілія')
-    patronymic = models.CharField(blank=True, max_length=30, verbose_name='По батькові')
-    position = models.CharField(blank=True, max_length=100, verbose_name='Посада')
+    name = models.CharField(blank=True, null=True, max_length=30, verbose_name="Ім'я")
+    surname = models.CharField(blank=True, null=True, max_length=30, verbose_name='Фамілія')
+    patronymic = models.CharField(blank=True, null=True, max_length=30, verbose_name='По батькові')
+    position = models.CharField(blank=True, null=True, max_length=100, verbose_name='Посада')
     # prosecutors_office = models.ForeignKey(ProsecutorsOffice, blank=True, null=True, verbose_name='Прокуратура', related_name='prosecutors_office')
     # department = models.ForeignKey(Department, blank=True, null=True, verbose_name='Управління', related_name='department')
     # division = models.ForeignKey(Division, blank=True, null=True, verbose_name='Відділ', related_name='division')
-    work_telephone = models.CharField(blank=True, max_length=100, verbose_name='Службовий телефон', default='-',
+    work_telephone = models.CharField(blank=True, null=True, max_length=100, verbose_name='Службовий телефон', default='-',
                                       help_text='Номера разделяются символом ;')
-    private_telephone = models.CharField(blank=True, max_length=100, verbose_name='Мобільний телефон', default='-',
+    private_telephone = models.CharField(blank=True, null=True, max_length=100, verbose_name='Мобільний телефон', default='-',
                                          help_text='Номера разделяются символом ;')
 
     def __str__(self):
@@ -25,7 +25,7 @@ class Division(models.Model):
     """
     Модель для отделов
     """
-    name = models.CharField(max_length=300, verbose_name='Отдел')
+    name = models.CharField(max_length=300, verbose_name='Відділ')
     employees = models.ManyToManyField('Employee', blank=True, null=True, verbose_name='Працівники')
 
     def __str__(self):
@@ -36,8 +36,8 @@ class Department(models.Model):
     """
     Модель для управлений
     """
-    name = models.CharField(max_length=200, verbose_name='Управление')
-    division = models.ManyToManyField(Division, blank=True, null=True, verbose_name='Отдел')
+    name = models.CharField(max_length=200, verbose_name='Управління')
+    division = models.ManyToManyField(Division, blank=True, null=True, verbose_name='Відділ')
     employees = models.ManyToManyField('Employee', blank=True, null=True, verbose_name='Працівники',
                                        help_text='Работники без отдела, работающие в управлении <br>')
 
@@ -50,13 +50,15 @@ class ProsecutorsOffice(models.Model):
     Модель для прокуратур
     """
     name = models.CharField(max_length=300, verbose_name='Прокуратура')
+    department = models.ManyToManyField(Department, blank=True, null=True, verbose_name='Управління')
+    division = models.ManyToManyField(Division, blank=True, null=True, verbose_name='Відділ')
     employees = models.ManyToManyField('Employee', blank=True, null=True, verbose_name='Працівники',
                                        help_text='Работники работающие напрямую в прокуратуре без управления и отдела<br>')
-    department = models.ManyToManyField(Department, blank=True, null=True, verbose_name='Управління')
-    address = models.CharField(blank=True, max_length=200, verbose_name='Адрес')
-    email_inside = models.EmailField(blank=True, verbose_name='Внутрішній e-mail')
-    email_outside = models.EmailField(blank=True, verbose_name='Зовнішній e-mail')
-    tel_cod = models.CharField(blank=True, max_length=7, verbose_name='Телефонный код')
+    tel_cod = models.CharField(blank=True, null=True, max_length=7, verbose_name='Телефонный код')
+    address = models.CharField(blank=True, null=True, max_length=200, verbose_name='Адрес')
+    email_inside = models.EmailField(blank=True, null=True, verbose_name='Внутрішній e-mail')
+    email_outside = models.EmailField(blank=True, null=True, verbose_name='Зовнішній e-mail')
+
 
     def __str__(self):
         return self.name
