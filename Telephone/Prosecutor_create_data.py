f434118576219242bee695a41a0cd77ee8599d08
stuqs <nanorhist@ukr.net>
2015-01-23 16:51:42 +0200

    Add aditional parameters in model to sort printed data

--- Telephone/Prosecutor_create_data.py
+++ Telephone/Prosecutor_create_data.py
@@ -1,35 +1,46 @@
-from Telephone.models import ProsecutorsOffice, Department, Division, Employee
+from Telephone.models import ProsecutorsOffice, Department, Division, Employee, Position
 from django.contrib.auth.models import User
 
 
 print(User.objects.create_superuser(username='admin', password='admin', email='A@A.nl'))
 
+##################################################################
 
-e1 = Employee(name='Ярослав', surname='Заворотний', patronymic='Семенович', position='заступник начальника управління',
+p1 = Position(position='заступник начальника управління')
+p1.save()
+
+p2 = Position(position='начальник відділу')
+p2.save()
+
+p3 = Position(position='прокурор')
+p3.save()
+
+##################################################################
+
+e1 = Employee(name='Ярослав', surname='Заворотний', patronymic='Семенович', position=p1,
               private_telephone='050-336-08-57')
 e1.save()
 
-e2 = Employee(name='Максим', surname='Ракович', patronymic='Миколайович', position='начальник відділу',
+e2 = Employee(name='Максим', surname='Ракович', patronymic='Миколайович', position=p2,
               work_telephone='1500;731-98-62',
               private_telephone='067-484-46-84')
 e2.save()
 
-e3 = Employee(name='Сергій', surname='Бондарев', patronymic='Юрійович', position='прокурор',
+e3 = Employee(name='Сергій', surname='Бондарев', patronymic='Юрійович', position=p3,
               work_telephone='1514;731-99-63',
               private_telephone='743-69-98')
 e3.save()
 
-e4 = Employee(name='Олексій', surname='Котелевський', patronymic='Іванович', position='начальник відділу',
+e4 = Employee(name='Олексій', surname='Котелевський', patronymic='Іванович', position=p2,
               work_telephone='1105;731-98-67',
               private_telephone='097-307-11-25')
 e4.save()
 
-e5 = Employee(name='Вадим', surname='Шивцов', patronymic='Михайлович', position='начальник відділу',
+e5 = Employee(name='Вадим', surname='Шивцов', patronymic='Михайлович', position=p3,
               private_telephone='096-288-83-90')
 e5.save()
 
-
-
+##################################################################
 
 d1 = Division(name='1) Відділ організації представництва на захист інтересів громадянина або держави')
 d1.save()
@@ -45,14 +56,16 @@ d3.save()
 d4 = Division(name='4) Організаційно-методичний відділ')
 d4.save()
 
+##################################################################
 
 D = Department(name='VIII. УПРАВЛІННЯ представництва, захисту інтересів громадян та держави в суді')
 D.save()
 D.employees.add(e1)
 
+##################################################################
 
 PO = ProsecutorsOffice(name='АПАРАТ', address='м.Одеса, вул. Пушкінська, 3', email_outside='Odeska.obl@od.gp.gov.ua',
-                tel_cod='048')
+                       tel_cod='048')
 PO.save()
 PO.department.add(D)
 
--- Telephone/models.py
+++ Telephone/models.py
@@ -1,5 +1,83 @@
 from django.db import models
 
+#
+# class Employee(models.Model):
+#     """
+#     Модель для всех сотрудников
+#     """
+#     name = models.CharField(blank=True, null=True, max_length=30, verbose_name="Ім'я")
+#     surname = models.CharField(blank=True, null=True, max_length=30, verbose_name='Фамілія')
+#     patronymic = models.CharField(blank=True, null=True, max_length=30, verbose_name='По батькові')
+#     position = models.CharField(blank=True, null=True, max_length=100, verbose_name='Посада')
+#     work_telephone = models.CharField(blank=True, null=True, max_length=100, verbose_name='Службовий телефон', default='-',
+#                                       help_text='Номера разделяются символом ;')
+#     private_telephone = models.CharField(blank=True, null=True, max_length=100, verbose_name='Мобільний телефон', default='-',
+#                                          help_text='Номера разделяются символом ;')
+#
+#     def __str__(self):
+#         return "{} {} {}".format(self.name, self.surname, self.patronymic)
+#
+#     class Meta:
+#         # order_with_respect_to = 'position'
+#         pass
+#
+#
+#
+# class Division(models.Model):
+#     """
+#     Модель для отделов
+#     """
+#     name = models.CharField(max_length=300, verbose_name='Відділ')
+#     employees = models.ManyToManyField('Employee', blank=True, null=True, verbose_name='Працівники')
+#     email_outside = models.EmailField(blank=True, null=True, verbose_name='Зовнішній e-mail')
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class Department(models.Model):
+#     """
+#     Модель для управлений
+#     """
+#     name = models.CharField(max_length=200, verbose_name='Управління')
+#     division = models.ManyToManyField(Division, blank=True, null=True, verbose_name='Відділ')
+#     employees = models.ManyToManyField('Employee', blank=True, null=True, verbose_name='Працівники',
+#                                        help_text='Работники без отдела, работающие в управлении <br>')
+#     email_outside = models.EmailField(blank=True, null=True, verbose_name='Зовнішній e-mail')
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class ProsecutorsOffice(models.Model):
+#     """
+#     Модель для прокуратур
+#     """
+#     name = models.CharField(max_length=300, verbose_name='Прокуратура')
+#     department = models.ManyToManyField(Department, blank=True, null=True, verbose_name='Управління')
+#     division = models.ManyToManyField(Division, blank=True, null=True, verbose_name='Відділ')
+#     employees = models.ManyToManyField('Employee', blank=True, null=True, verbose_name='Працівники',
+#                                        help_text='Работники работающие напрямую в прокуратуре без управления и отдела<br>')
+#     tel_cod = models.CharField(blank=True, null=True, max_length=7, verbose_name='Телефонный код')
+#     address = models.CharField(blank=True, null=True, max_length=200, verbose_name='Адрес')
+#     email_inside = models.EmailField(blank=True, null=True, verbose_name='Внутрішній e-mail')
+#     email_outside = models.EmailField(blank=True, null=True, verbose_name='Зовнішній e-mail')
+#
+#     def __str__(self):
+#         return self.name
+
+
+from django.db import models
+
+
+class Position(models.Model):
+    """
+    Модель для всех должности сотрудников, нужна что бы производить сортировку при выводе из БД
+    """
+    position = models.CharField(max_length=100, verbose_name='Посада')
+
+    def __str__(self):
+        return self.position
 
 class Employee(models.Model):
     """
@@ -8,14 +86,17 @@ class Employee(models.Model):
     name = models.CharField(blank=True, null=True, max_length=30, verbose_name="Ім'я")
     surname = models.CharField(blank=True, null=True, max_length=30, verbose_name='Фамілія')
     patronymic = models.CharField(blank=True, null=True, max_length=30, verbose_name='По батькові')
-    position = models.CharField(blank=True, null=True, max_length=100, verbose_name='Посада')
-    work_telephone = models.CharField(blank=True, null=True, max_length=100, verbose_name='Службовий телефон', default='-',
-                                      help_text='Номера разделяются символом ;')
-    private_telephone = models.CharField(blank=True, null=True, max_length=100, verbose_name='Мобільний телефон', default='-',
-                                         help_text='Номера разделяются символом ;')
+    position = models.ForeignKey(Position, blank=True, null=True, verbose_name='Посада')
+    work_telephone = models.CharField(blank=True, null=True, max_length=100, verbose_name='Службовий телефон',
+                                      default='-', help_text='Номера разделяются символом ;')
+    private_telephone = models.CharField(blank=True, null=True, max_length=100, verbose_name='Мобільний телефон',
+                                         default='-', help_text='Номера разделяются символом ;')
 
     def __str__(self):
         return "{} {} {}".format(self.name, self.surname, self.patronymic)
+
+    class Meta:
+        order_with_respect_to = 'position'
 
 
 class Division(models.Model):
@@ -58,8 +139,5 @@ class ProsecutorsOffice(models.Model):
     email_inside = models.EmailField(blank=True, null=True, verbose_name='Внутрішній e-mail')
     email_outside = models.EmailField(blank=True, null=True, verbose_name='Зовнішній e-mail')
 
-
     def __str__(self):
         return self.name
-
-
