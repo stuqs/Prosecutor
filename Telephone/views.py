a47fe073fb0b16ba23ba65f555fa1be5ccac736b
stuqs <nanorhist@ukr.net>
2015-01-23 10:48:16 +0200

    Refresh todo list, reftor template

--- Telephone/views.py
+++ Telephone/views.py
@@ -13,4 +13,5 @@ def main_page(request):
     #     #Сделать проверку на безопастность (наличие только чисел с - и ;)
     #      emp.work_telephone = "<br>".join(emp.work_telephone.split(';'))
     #      emp.private_telephone = "<br>".join(emp.private_telephone.split(';'))
-    return render(request, 'main.html', {'prosecutorsoffices': prosecutorsoffices, 'employees': employees})
+    return render(request, 'main.html', {'prosecutorsoffices': prosecutorsoffices, 'employees': employees,
+                                         'table_header': 'table_header.html', 'table_loop': 'table_loop.html'})
