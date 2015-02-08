9c2a341afb98e7c1f2cb64f77297b5706aec879f
stuqs <nanorhist@ukr.net>
2015-02-08 20:12:10 +0200

    Add CSS, nice tables

--- Prosecutor/urls.py
+++ Prosecutor/urls.py
@@ -1,16 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from Telephone.views import *
-from django.views.generic.base import TemplateView
+from django.conf import settings
 
 # admin.autodiscover()
 
-
-
-
 urlpatterns = patterns('',
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', main_page),
-    url(r'^t/$', TemplateView.as_view(template_name='main1.html'))
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^$', main_page),
+                       url(r'^t/$', view_for_test),
+                       # url(r'^t/$', TemplateView.as_view(template_name='main1.html'))
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+                            (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'./media/'}),
+                            )
--- Telephone/views.py
+++ Telephone/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from Telephone.models import *
 
 def main_page(request):
+    print(request.GET)
+    # print(request.GET['pros_office'])
+    prosecutorsoffices = ProsecutorsOffice.objects.all()
+    # if request.GET['pros_office']:
+    #     prosecutorsoffices = ProsecutorsOffice.objects.filter(id=request.GET['pros_office'])
+    #
+    # def sort_employees(self):
+    #     return sorted(self.employees.all(), key=(lambda x=self.employees.all(): x.position.weight), reverse=True)
+
+# name
+# telephone
+# pros_office
 
 
-    prosecutorsoffices = ProsecutorsOffice.objects.order_by('name')
 
-
-
-    # for emp in employees:
-    #     #Сделать проверку на безопастность (наличие только чисел с - и ;)
-    #      emp.work_telephone = "<br>".join(emp.work_telephone.split(';'))
-    #      emp.private_telephone = "<br>".join(emp.private_telephone.split(';'))
-
-    return render(request, 'main.html', {'prosecutorsoffices': prosecutorsoffices, 'table_header': 'table_header.html',
+    return render(request, 'main2.html', {'prosecutorsoffices': prosecutorsoffices, 'table_header': 'table_header.html',
                                          'table_loop': 'table_loop.html'})
 
+
+def view_for_test(request, *args, **kwargs):
+
+    print(request.GET)
+    # return HttpResponseRedirect('/')
+    prosecutorsoffices = ProsecutorsOffice.objects.all()
+    return render(request, 'main1.html', {'prosecutorsoffices': prosecutorsoffices})
