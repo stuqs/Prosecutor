20e8c2482ecdddefe62e7831a0a4baebc8e46f65
stuqs <nanorhist@ukr.net>
2015-02-27 16:44:26 +0200

    tree function

--- Telephone/views.py
+++ Telephone/views.py
@@ -14,12 +14,12 @@ def main_with_filter(request):
             field_query = {FILTER[k]: v}
             employee_list = employee_list.filter(**field_query)
     employees_dict = create_employee_structure(employee_list)
-    po_list = ProsecutorsOffice.objects.all()
-    department_list = Department.objects.all()
-    division_list = Division.objects.all()
+    # po_list = ProsecutorsOffice.objects.all()
+    # department_list = Department.objects.all()
+    # division_list = Division.objects.all()
     return render(request, 'main.html', {'employees_dict': employees_dict, 'filter_header': 'filter_header.html',
-                                         'table_header': 'table_header.html', 'table_loop': 'table_loop.html',
-                                         'po_list': po_list, 'department_list': department_list, 'division_list': division_list})
+                                         'table_header': 'table_header.html', 'table_loop': 'table_loop.html'})
+                                         # 'po_list': po_list, 'department_list': department_list, 'division_list': division_list})
 
 
 def tree_structure(request):
@@ -28,9 +28,37 @@ def tree_structure(request):
 
 
 def test_f(request, po=None, department=None, division=None):
+    employee_list = Employee.objects.all()
+    print(po)
+    print(department)
+    print(division)
+
+
     if po:
-        print(po)
+        # print(po)
+        # print()
+        employee_list.filter(prosecutors_office__name__icontains=po)
     if department:
-        print(department)
+        # print(department)
+        # print()
+        employee_list.filter(department__name__icontains=department)
     if division:
-        print(division)
+        # print(division)
+        employee_list.filter(division__name__icontains=division)
+    employees_dict = create_employee_structure(employee_list)
+    # return render(request, 'main.html', {'employees_dict': employees_dict})
+    return render(request, 'main.html', {'employees_dict': employees_dict, 'filter_header': 'filter_header.html',
+                                         'table_header': 'table_header.html', 'table_loop': 'table_loop.html',})
+
+
+
+
+
+
+
+
+
+
+
+
+
