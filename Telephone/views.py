2c93af6b64b03718d59500ff00e35859d0feab91
stuqs <nanorhist@ukr.net>
2015-03-03 17:25:56 +0200

    -

--- Telephone/views.py
+++ Telephone/views.py
@@ -9,18 +9,24 @@ def main_with_filter(request):
     """
     Main page with filter
     """
-    employee_list = Employee.objects.all()
-    for k, v in request.GET.items():
-        if FILTER.get(k) and v:
-            field_query = {FILTER[k]: v}
-            employee_list = employee_list.filter(**field_query)
-    employees_dict = create_employee_structure(employee_list)
     po_list = ProsecutorsOffice.objects.all()
     department_list = Department.objects.all()
     division_list = Division.objects.all()
-    return render(request, 'main.html', {'employees_dict': employees_dict, 'filter_header': 'filter_header.html',
-                                         'table_header': 'table_header.html', 'table_loop': 'table_loop.html',
-                                         'po_list': po_list, 'department_list': department_list, 'division_list': division_list})
+    if not request.GET:
+        return render(request, 'main.html', {'employees_dict': {}, 'filter_header': 'filter_header.html',
+                                             'table_header': 'table_header.html', 'table_loop': 'table_loop.html',
+                                             'po_list': po_list, 'department_list': department_list, 'division_list': division_list})
+    else:
+        employee_list = Employee.objects.all()
+        for k, v in request.GET.items():
+            if FILTER.get(k) and v:
+                field_query = {FILTER[k]: v}
+                employee_list = employee_list.filter(**field_query)
+        employees_dict = create_employee_structure(employee_list)
+
+        return render(request, 'main.html', {'employees_dict': employees_dict, 'filter_header': 'filter_header.html',
+                                             'table_header': 'table_header.html', 'table_loop': 'table_loop.html',
+                                             'po_list': po_list, 'department_list': department_list, 'division_list': division_list})
 
 
 def tree_structure(request):
